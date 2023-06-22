56091c63cf3c7b9c59d8bc4ebb7aacf43c4545e3
Ricardo Matias <ricardo.matias0@gmail.com>
2023-06-22 10:41:07 +0200

    Fix map method

--- src/common/math.cpp
+++ src/common/math.cpp
@@ -3,10 +3,22 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-namespace sonovolt::math {
+namespace sonovolt::math
+{
 
-template <typename T> T map(T s, T a1, T a2, T b1, T b2) { return b1 + (s - a1) * (b2 - b1) / (a2 - a1); }
-template <typename T> T clamp(T v, T min, T max) { return MAX(MIN(max, v), v); }
+template <typename T>
+T map(T s, T a1, T a2, T b1, T b2)
+{
+    return b1 + (b2 - b1) * ((s - a1) / (a2 - a1));
+}
+template float map(float s, float a1, float a2, float b1, float b2);
+
+template <typename T>
+T clamp(T v, T min, T max)
+{
+    return MAX(MIN(max, v), v);
+}
 template uint8_t clamp(uint8_t v, uint8_t min, uint8_t max);
+template float clamp(float v, float min, float max);
 
-} // namespace sonovolt
+} // namespace sonovolt::math
